bdc3c9922f88bdb4d0f468ed662d22584aa8d085
linyuchen <lin.yu.chen@hotmail.com>
2025-05-26 20:48:06 +0800

    fix: Members, UserList

--- src/components/Detail/Members.vue
+++ src/components/Detail/Members.vue
@@ -1,12 +1,10 @@
 <template>
   <div>
-    <template>
-      <el-table :data="tableData" height="250" style="width: 100%">
-        <el-table-column prop="userid" label="用户id" width="180" />
-        <el-table-column prop="name" label="会员姓名" width="180" />
-        <el-table-column prop="account" label="会员账户" />
-      </el-table>
-    </template>
+    <el-table :data="tableData" height="250" style="width: 100%">
+      <el-table-column prop="userid" label="用户id" width="180" />
+      <el-table-column prop="name" label="会员姓名" width="180" />
+      <el-table-column prop="account" label="会员账户" />
+    </el-table>
   </div>
 </template>
     <script  setup>
--- src/components/Detail/UserList.vue
+++ src/components/Detail/UserList.vue
@@ -1,15 +1,14 @@
 <template>
   <div>
-    <template>
-      <el-table :data="tableData" height="250" style="width: 100%">
-        <el-table-column prop="date" label="Date" width="180" />
-        <el-table-column prop="name" label="Name" width="180" />
-        <el-table-column prop="address" label="Address" />
-      </el-table>
-    </template>
+    <el-table :data="tableData" height="250" style="width: 100%">
+      <el-table-column prop="date" label="Date" width="180" />
+      <el-table-column prop="name" label="Name" width="180" />
+      <el-table-column prop="address" label="Address" />
+    </el-table>
   </div>
 </template>
-  <script setup>
+
+<script setup>
   import {ref} from "vue";
 
   const tableData = ref([
@@ -49,11 +48,7 @@
         address: 'No. 189, Grove St, Los Angeles',
       },
     ])
-  </script>
-
-
-
-
+</script>
 
 <style lang="scss" scoped>
 </style>
